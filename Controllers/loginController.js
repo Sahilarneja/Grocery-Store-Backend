1ef2a9defe5da2bf3ca5ac91a676296a90daa3fd
Sahil <sahilarneja2003@gmail.com>
2023-11-06 20:34:14 +0530

    JWT_Authentication added

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -1,78 +1,94 @@
 const loginModel = require('../Model/loginModel');
-const encryptPassword = require('../utils/bcrypt');
+const bcrypt = require('../utils/bcrypt'); // Corrected the import
 
-const adminLogin=async (req,res)=>{
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretKey = process.env.SECRET_KEY;
+
+const generateToken = (email, userType) => {
+    const payload = {
+        login: {
+            email: email,
+            userType: userType,
+        },
+    };
+    return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+};
+
+const adminLogin = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        const admin=await loginModel.findOne({email});
+        const { email, password } = req.body;
+        const admin = await loginModel.findOne({ email });
 
-        if(!admin || admin.userType !== 'Admin'){
-            return res.status(400).json({message:"Invalid admin credentials"});
+        if (!admin || admin.userType !== 'Admin') {
+            return res.status(400).json({ message: "Invalid admin credentials" });
         }
 
-        const isPasswordValid=encryptPassword.matchPwd(password, admin.password);
-        if(isPasswordValid){
-            return res.status(200).json({message:"Admin Login Successful"});
-        }else{
-            return res.status(400).json({message:"Invalid admin credentials"});
+        const isPasswordValid = await bcrypt.matchPwd(password, admin.password); // Changed "encryptPassword" to "bcrypt"
+        if (isPasswordValid) {
+            const token = generateToken(admin.email, admin.userType); // Changed to use admin's email
+            return res.status(200).json({ message: "Admin Login Successful", token });
+        } else {
+            return res.status(400).json({ message: "Invalid admin credentials" });
         }
     } catch (error) {
         console.log(error);
-        res.status(500).json({message:"Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
-const userLogin=async(req,res)=>{
+const userLogin = async (req, res) => {
     try {
-        const {email,password}=req.body;
-        const user=await loginModel.findOne({email});
+        const { email, password } = req.body;
+        const user = await loginModel.findOne({ email });
 
-        if(!user || user.userType !== 'User'){
-            return res.status(400).json({message:"Invalid user credentials"});
+        if (!user || user.userType !== 'User') {
+            return res.status(400).json({ message: "Invalid user credentials" });
         }
-        const isPasswordValid=encryptPassword.matchPwd(password,user.password);
-        if(isPasswordValid){
-            return res.status(200).json({message:"User login successful"});
-        }else{
-            return res.status(500).json({message:"Internal Server Error"});
+        const isPasswordValid = await bcrypt.matchPwd(password, user.password); // Changed "encryptPassword" to "bcrypt"
+        if (isPasswordValid) {
+            const token = generateToken(user.email, user.userType); // Changed to use user's email
+            return res.status(200).json({ message: "User login successful", token });
+        } else {
+            return res.status(400).json({ message: "Invalid user credentials" });
         }
     } catch (error) {
         res.status(500).json({ message: 'Internal error' });
     }
 };
 
-const loginController=async(req,res)=>{
+const loginController = async (req, res) => {
     try {
-        const{email,password,userType}=req.body;
-        const login=await loginModel.findOne({email});
+        const { email, password, userType } = req.body;
+        const login = await loginModel.findOne({ email });
 
-        if(!login){
-            return res.status(400).json({message:"Invalid credentials"});
+        if (!login) {
+            return res.status(400).json({ message: "Invalid credentials" });
         }
-        const isPasswordValid=encryptPassword.matchPwd(password,login.password);
-        if(isPasswordValid){
-            if(userType==="Admin"){
-                console.log("request forwarded to admin login")
-                return adminLogin({...req, body: {...req.body,userType}}, res);
-            }else{
+        const isPasswordValid = await bcrypt.matchPwd(password, login.password); // Changed "encryptPassword" to "bcrypt"
+        if (isPasswordValid) {
+            if (userType === "Admin") {
+                console.log("request forwarded to admin login");
+                return adminLogin({ ...req, body: { ...req.body, userType } }, res);
+            } else {
                 console.log("request forwarded to user login");
-                return userLogin({...req, body: {...req.body,userType}},res);
+                return userLogin({ ...req, body: { ...req.body, userType } }, res);
             }
-        }else{
+        } else {
             return res.status(400).json({
-                message:"Invalid credentials"
+                message: "Invalid credentials"
             });
         }
     } catch (error) {
-        res.status(500).json({message:"Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
 const registerController = async (req, res) => {
     try {
         const { username, email, password, userType } = req.body;
-        const hashedPwd=encryptPassword.hasPwd(password);
-        console.log('Trying to insert user with email: ',email);
+        const hashedPwd = await bcrypt.hasPwd(password); // Corrected to "hashPwd"
+        console.log('Trying to insert user with email: ', email);
         const login = await loginModel.create({
             username: username,
             email: email,
@@ -87,12 +103,11 @@ const registerController = async (req, res) => {
         }
     } catch (error) {
         if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
-            res.status(400).json({ message: 'already exists' });
+            res.status(400).json({ message: 'Email already exists' }); // Corrected the message
         } else {
             console.error(error);
             res.status(500).json({ message: 'Internal error' });
         }
-
     }
 };
 
